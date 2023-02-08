bee64afc8f66c3d59327d9da7626f6091ea5b37e
DrKarambit <43848302+DrKarambit@users.noreply.github.com>
2023-02-08 20:58:14 +0100

    Add roulette_wheel_selection to genetic algorithm

--- metaopts/algorithms/ga.py
+++ metaopts/algorithms/ga.py
@@ -14,8 +14,13 @@ def ga(model_weights, model_fitness, population_size):
                 mw.assign(p[i])
             fitness_values[i].assign(model_fitness())
 
-    def selection():
-        pass
+    @tf.function
+    def roulette_wheel_selection():
+        print('Tracing roulette_wheel_selection...')
+        normalized = tf.reduce_sum(fitness_values) / fitness_values
+        extended = tf.repeat(tf.expand_dims(normalized, axis=0), population_size, axis=0)
+        selected = tf.random.categorical(tf.math.log(extended), 2, dtype=tf.int32)
+        selected_individuals.assign(selected)
 
     def crossover():
         pass
@@ -26,3 +31,4 @@ def ga(model_weights, model_fitness, population_size):
 
     population = [tf.Variable(tf.repeat(tf.expand_dims(weights, axis=0), population_size, axis=0)) for weights in model_weights]
     fitness_values = tf.Variable(tf.zeros(population_size, dtype=tf.float32))
+    selected_individuals = tf.Variable(tf.zeros((population_size, 2), dtype=tf.int32))
