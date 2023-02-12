36a5a9ab8ff1409a464db7c8751fd1a67311a17c
DrKarambit <43848302+DrKarambit@users.noreply.github.com>
2023-02-12 20:13:22 +0100

    Update utility functions

--- metaopts/__init__.py
+++ metaopts/__init__.py
@@ -1,8 +1,12 @@
-from .utilities.fitness import create_fitness
+from .utilities.fitness import create_fitness_function, update_fitness_values
+from .utilities.population import create_population, sort_population
 from .algorithms.ga import ga
 
 
 __all__ = [
-    'create_fitness',
+    'create_fitness_function',
+    'update_fitness_values',
+    'create_population',
+    'sort_population',
     'ga',
-]
+    ]
---
+++ metaopts/utilities/population.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+
+
+def create_population(model_weights, population_size, transfer_learning=False):
+    """
+    Creates a population of individuals (candidate solutions).
+
+    Args:
+        model_weights: `list` of `tf.Variable` - List of model weights.
+        population_size: `int` - Number of individuals in the population.
+        transfer_learning: `bool` - Whether to copy current model weights or generate random ones.
+    """
+
+    if (transfer_learning):
+
+        # Return a population of individuals with the same weights as the model
+        return [tf.Variable(tf.repeat([weights], population_size, axis=0)) for weights in model_weights]
+
+    # Return a population of individuals with random weights
+    return [tf.Variable(tf.repeat([tf.random.normal(weights.shape, 0, 0.1)], population_size, axis=0)) for weights in model_weights]
+
+
+def sort_population(population, fitness_values):
+    """
+    Sorts the population in ascending order based on fitness values.
+
+    Args:
+        population: `list` of `tf.Variable` - List of population weights.
+        fitness_values: `tf.Variable` - Fitness values of the population.
+    """
+
+    # Print debug information
+    print('Tracing sort_population...')
+
+    # Sort fitness values in ascending order
+    order = tf.argsort(fitness_values)
+
+    # Assign new order to fitness values and the population
+    fitness_values.assign(tf.gather(fitness_values, order, axis=0))
+    for p in population:
+        p.assign(tf.gather(p, order, axis=0))
+
+
+sort_population = tf.function(sort_population)
